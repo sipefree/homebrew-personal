6a57667a7ef6db7f0c5d6db184929d8a83a24ade
Simon Free <sfree@apple.com>
2024-09-10 16:05:53 -0700

    Fixed typo

--- xcode-color-assets.rb
+++ xcode-color-assets.rb
@@ -1,7 +1,7 @@
 class XcodeColorAssets < Formula
     desc "Create (dark mode compatible) color assets for Xcode programmatically from a CSS-like textfile"
     homepage "https://github.com/sfree/xcode-color-assets"
-    url "https://github.com/sfree/xcode-color-assets/releases/download/1.0.0/xcode-color-assets-1.0.0.tar.gz"
+    url "https://github.com/sipefree/xcode-color-assets/releases/download/1.0.0/xcode-color-assets-1.0.0.tar.gz"
     sha256 "943b0d4b398d59e7deb182430374a6b9a38934a05fefe654590cca51d0054a73"
   
     def install
